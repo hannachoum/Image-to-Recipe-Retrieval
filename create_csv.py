4cff02c9d4d5110ff22952616d483278ee6d282b
Ribal Teeny <ribal.teeny@raie.polytechnique.fr>
2024-03-05 15:05:04 +0100

    first end to end test

--- create_csv.py
+++ create_csv.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from transformers import pipeline
 from tqdm import tqdm
@@ -5,8 +6,8 @@ import torch
 import csv
 
 # Charger le fichier CSV
-input_path = pd.read_csv('data/Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
-output_csv_path = 'bert_result_multi.csv'
+input_path = pd.read_csv('Food Ingredients and Recipe Dataset with Image Name Mapping.csv')
+output_csv_path = 'export.csv'
 MAX_LENGTH = 77
 
 # Configuration du dispositif pour l'exécution du modèle
@@ -15,13 +16,13 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 # Initialiser le modèle de résumé
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn", device=0 if device == "cuda" else -1)
 nb_toolong = 0
-
-count=0
+idx = 0
 
 with open(output_csv_path, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     # Écrire l'en-tête du fichier CSV
     writer.writerow(['Title', 'Ingredients', 'Summary', 'Image_Name', 'Cleaned_Ingredients'])
+    not_str = {}
 
     for _, row in tqdm(input_path.iterrows(), desc="Processing descriptions"):
         description = row['Instructions']
@@ -30,43 +31,48 @@ with open(output_csv_path, mode='w', newline='', encoding='utf-8') as file:
         image_name = row.get('Image_Name', '')  # Idem
         cleaned_ingredients = row.get('Cleaned_Ingredients', '')  # Idem
 
-        count+=1
-        
-        if count == 100:
-            break
-        
-        else: 
-            # Générer le résumé
-            if len(description) < 4000:
-                if len(description) < MAX_LENGTH:
-                    summary = description
-                else:
-                    summary_result = summarizer(description, max_length=MAX_LENGTH, min_length=10, do_sample=False)
-                    #print("1")
-                    #print (summary_result)
-                    summary = summary_result[0]['summary_text']
-                    #print(summary)
-            elif len(description)/2 < 4000:
-                first_part = description[:int(len(description) / 2)]
-                second_part = description[int(len(description) / 2):int(len(description))]
-
-
-                first_summary = summarizer(first_part, max_length=MAX_LENGTH, min_length=10, do_sample=False)
-                #print("first 1",first_summary)
-                first_summary = first_summary[0]['summary_text']
-                #print("first 2",first_summary)
-
-                second_summary = summarizer(second_part, max_length=MAX_LENGTH, min_length=10, do_sample=False)
-                #print(second_summary)
-                second_summary = second_summary[0]['summary_text']
-                #print(second_summary)
-                summary = first_summary + " " + second_summary
-                #print(second_summary)
-            else:
-                nb_toolong += 1
+        # print(len(description), type(description))
+        # print(description)
+        # Générer le résumé
+        if not isinstance(description, str) or len(str(description))==0:
+            not_str[idx] = [image_name, description]
+            idx+=1
+            continue
+        if len(description) < 4000:
+            if len(description.split()) < MAX_LENGTH:
                 summary = description
+            else:
+                summary_result = summarizer(description, max_length=MAX_LENGTH, min_length=40, do_sample=False)
+                # print ("summary_res", summary_result)
+                summary = summary_result[0]['summary_text']
+                # print("summary", summary)
 
-            # Écrire la ligne dans le fichier CSV
-            writer.writerow([title, ingredients, summary, image_name, cleaned_ingredients])
+        # case where input too long to smmerize
+        elif len(description)/2 < 4000:
+            first_part = description[:int(len(description) / 2)]
+            second_part = description[int(len(description) / 2):int(len(description))]
 
-#print(f"{nb_toolong} descriptions were too long for summarization.")
+
+            first_summary = summarizer(first_part, max_length=MAX_LENGTH, min_length=10, do_sample=False)
+            #print("first 1",first_summary)
+            first_summary = first_summary[0]['summary_text']
+            #print("first 2",first_summary)
+
+            second_summary = summarizer(second_part, max_length=MAX_LENGTH, min_length=10, do_sample=False)
+            #print(second_summary)
+            second_summary = second_summary[0]['summary_text']
+            #print(second_summary)
+            summary = first_summary + " " + second_summary
+            #print(second_summary)
+        else:
+            nb_toolong += 1
+            summary = description
+
+        # Écrire la ligne dans le fichier CSV
+        idx+=1
+        writer.writerow([title, ingredients, summary, image_name, cleaned_ingredients])
+
+print(f"{nb_toolong} descriptions were too long for summarization.")
+
+with open("bad_instructions.txt", "w") as f:
+    json.dump(not_str, f)
